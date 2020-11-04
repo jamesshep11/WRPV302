e49a4dfcd5ce90350e832b157004824719cc5a62
James <james@avochoc.com>
2020-11-04 09:48:09 +0200

    PetDetails Activity Designed

---
+++ 48HourAssignment/app/src/main/java/com/example/a48hourassignment/PetDetails.java
@@ -0,0 +1,14 @@
+package com.example.a48hourassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PetDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet_details);
+    }
+}
