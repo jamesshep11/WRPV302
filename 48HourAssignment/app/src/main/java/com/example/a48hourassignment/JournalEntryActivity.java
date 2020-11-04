db47579dae447e139e860a2667e7666041a39f0b
James <james@avochoc.com>
2020-11-04 19:47:34 +0200

    Take and save photos

--- 48HourAssignment/app/src/main/java/com/example/a48hourassignment/JournalEntryActivity.java
+++ 48HourAssignment/app/src/main/java/com/example/a48hourassignment/JournalEntryActivity.java
@@ -1,9 +1,16 @@
 package com.example.a48hourassignment;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -11,7 +18,11 @@ import android.widget.Spinner;
 
 import com.example.a48hourassignment.PubSubBroker.Broker;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 
 public class JournalEntryActivity extends AppCompatActivity {
@@ -24,6 +35,7 @@ public class JournalEntryActivity extends AppCompatActivity {
 
     Entry thisEntry;
     int pos;
+    String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,8 +69,62 @@ public class JournalEntryActivity extends AppCompatActivity {
     }
 
     public void changeImage(View view){
-
+        dispatchTakePictureIntent();
     }
+
+    //region Take and Save Photo
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                new AlertDialog.Builder(this)
+                        .setTitle("Error")
+                        .setMessage("Could not create image file")
+                        .show();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imgEntryImage.setImageBitmap(imageBitmap);
+        }
+    }
+    //endregion
 
     public void onSaveClicked(View view){
         thisEntry.setImage(Integer.parseInt(imgEntryImage.getTag().toString()));
