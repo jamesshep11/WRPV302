676f16fc80a1a8076450a1606a9ae5a9c108938a
James <james@avochoc.com>
2020-11-19 11:32:25 +0200

    created new branch

--- PubSubBroker/src/PubSubBroker/Subscriber.java
+++
@@ -1,8 +0,0 @@
-package PubSubBroker;
-
-import java.util.Map;
-
-@FunctionalInterface
-public interface Subscriber {
-    void onPublished(Object publisher, String topic, Map<String,Object> params);
-}
